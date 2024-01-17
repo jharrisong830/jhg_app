6fe275d248970293df449bca395f6b89be3626db
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-01-17 17:08:35 -0500

    added json to html converter tool!

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import flask
 import json
 from time import strptime
-import requests
 
 DATE_FORMAT = "%Y-%m-%d"
 
@@ -41,6 +40,37 @@ def privacy():
 def get_post_date(p: dict[str:str]):
     """returns a date object for the date of the given post"""
     return strptime(p["date"], DATE_FORMAT)
+
+
+# preliminary json -> table converter, DO NOT PUBLISH
+@app.route("/convert", methods=["GET", "POST"])
+def convert():
+    if flask.request.method == "GET":
+        return flask.render_template("pages/convert.html", tableCells="<p class=\"text-center lead text-body-emphasis\">Enter a valid JSON object above, and it will render in HTML here!</p>")
+    elif flask.request.method == "POST":
+        try:
+            data = flask.request.form["jsonInp"]
+            jsonObj = json.loads(data)
+            tableString = renderObject(jsonObj)
+            return flask.render_template("pages/convert.html", tableCells=tableString)
+        except:
+            return flask.render_template("pages/convert.html", tableCells="<p class=\"text-center lead text-body-emphasis\">There was an error parsing your JSON text. Please try again, and insure your input has valid syntax.</p>")
+
+
+def renderObject(obj) -> str:
+    if isinstance(obj, dict):
+        oKeys = obj.keys()
+        tableString = f"<table class=\"table table-light table-hover table-bordered\"> <tr> <th>[obj]</th> {"".join([f'<th>{x}</th>' for x in oKeys])} </tr>" # adding header row
+        tableString += f"<tr> <td></td> {"".join([f'<td>{renderObject(obj[x])}</td>' for x in oKeys])} </tr> </table>"
+        return tableString
+    elif isinstance(obj, list):
+        tableString = f"<table class=\"table table-light table-hover table-bordered\"> <tr> <th>[ind]</th> <th>[elem]</th> </tr>" # adding header row
+        for i in range(len(obj)):
+            tableString += f"<tr> <td>{i}</td> <td>{renderObject(obj[i])}</td> </tr>"
+        tableString += "</table>"
+        return tableString
+    else:
+        return str(obj)
 
 
 
