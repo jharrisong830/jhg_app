49489cde755d86408a552b9744dc74028f6a8dee
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-01-17 17:13:58 -0500

    f-string fix for python pre-3.12

--- api/index.py
+++ api/index.py
@@ -60,7 +60,7 @@ def convert():
 def renderObject(obj) -> str:
     if isinstance(obj, dict):
         oKeys = obj.keys()
-        tableString = f"<table class=\"table table-light table-hover table-bordered\"> <tr> <th>[obj]</th> {"".join([f'<th>{x}</th>' for x in oKeys])} </tr>" # adding header row
+        tableString = f"<table class=\"table table-light table-hover table-bordered\"> <tr> <th>[obj]</th> {''.join([f'<th>{x}</th>' for x in oKeys])} </tr>" # adding header row
         tableString += f"<tr> <td></td> {"".join([f'<td>{renderObject(obj[x])}</td>' for x in oKeys])} </tr> </table>"
         return tableString
     elif isinstance(obj, list):
